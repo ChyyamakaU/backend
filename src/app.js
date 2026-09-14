587f808a542dbcc0ed8afa8a9529e3bec1ad6318
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-14 22:51:40 +0100

    ft: add about,  home and contact paths

--- src/app.js
+++ src/app.js
@@ -7,12 +7,36 @@ const app = express ();
 app.use(express.json());
 
 // routes
-app.get("/", (req, res)=> {
-res.json({
+app.get("/",  (req, res)=> {
+res.status(200).json({
     "status": "sucess",
     "message": "Welcome, API is running"
 });
 
 });
 
+app.get("/home", (req, res)=> {
+res.status(201).json({
+    "status": "sucess",
+    "message": "Welcome to home"
+});
+
+});
+
+
+app.get("/about", (req, res)=> {
+res.status(200).json({
+    "status": "sucess",
+    "message": "Welcome to About"
+});
+
+});
+
+app.get("/contact", (req, res)=> {
+res.status().json({
+    "status": "sucess",
+    "message": "Welcome to contact"
+});
+
+});
 module.exports = app;
